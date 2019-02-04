5ae591b3bdcacdae08f1cef4ad1f076b45aeadf5
DamasoDaniel <damasodt8a@gmail.com>
2019-02-04 00:26:34 -0600

    Se creó un arreglo con los datos de conexión a la bd y se creó la carpeta controller

--- config/bd.php
+++ config/bd.php
@@ -1,5 +1,14 @@
 <?php
-
 	
+	return array(
+		'driver' => 'mysql',
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => '1123MASTERkey58'
+		'database' => 'cms',
+		'charset' => 'utf8'
+
+	);
+
 
 ?>
