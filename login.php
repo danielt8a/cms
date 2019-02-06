eacd995de864b164160ad250925b8f45052a218b
DamasoDaniel <damasodt8a@gmail.com>
2019-02-05 21:39:39 -0600

    Creación del login del usuario con bootstrap

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<title>Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets\css\style.css">
+</head>
+<body>
+	<div class="container-fluid register-login">
+
+		<div class="row wrapper">
+			<div class="container__image">
+				<div class="image--points">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Start Contenedor -->
+	<div class="col-lg form-center d-flex justify-content-center align-items-center">
+		<div class="container-form">
+			<h1 class="register-login-h1">Acceso</h1>
+			<p class="register-login-p">¡Bienvenido, por favor ingresa tus credenciales!</p>
+			<!-- Inicio formulario -->
+			<form action="">
+				<div class="form-group">
+					<input type="text" id="username" name="username" class="form-control" required>
+					<label for="username" class="form-label">Usuario</label>
+				</div>
+			</form>
+			<!-- Fin Formulario -->
+		</div>
+		
+	</div>
+	
+</body>
+</html>
